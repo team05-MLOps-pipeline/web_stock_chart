06a9a9889b014a68683d4145e0b1de891c09fb40
humaningansalam <sanghun9634@gmail.com>
2023-10-28 11:50:24 +0900

    fetch add test file

--- test/ttweb04.py
+++ test/ttweb04.py
@@ -13,16 +13,22 @@ def get_stock_data(ticker, start_date, end_date):
     data = yf.download(ticker, start=start_date, end=end_date)
     return data
 
+
 # 주식 데이터를 차트로 그리는 함수를 정의합니다.
 def plot_stock_chart(data):
     fig = px.line(data, x=data.index, y="Close", title="Stock Price Chart")
     fig.update_layout(autosize=True)  # 그래프를 반응형으로 만듭니다.
     return fig
 
+
+
 @app.route('/')
 def dashboard():
     tickers = ['AAPL', 'MSFT', 'GOOGL', 'AMZN']  # 대시보드에 표시할 종목 리스트입니다.
     return render_template('stock_dashboard4.html', tickers=tickers)  # 종목 리스트를 HTML에 전달합니다.
+
+
+
 
 # 웹 서버 루트 경로에 접근하면 주식 차트를 보여주는 페이지를 반환합니다.
 @app.route('/chart/<ticker>')  # URL 경로에 종목 변수를 추가합니다.
